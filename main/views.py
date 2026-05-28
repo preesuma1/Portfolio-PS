319f0b2be0216de7e7929556a460a65c70eb6835
Preesuma Shrestha <preesuma1@gmail.com>
2026-05-28 23:04:25 +0545

    feat: create view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib import admin
+from django.urls import path
+from main.views import home
 
-# Create your views here.
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+]
